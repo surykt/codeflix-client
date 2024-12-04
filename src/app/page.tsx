69ca4fdcc574446d179b261e7120cb344e2e9b45
Carlos Cardoso <puro_cros@hotmail.com>
2024-12-04 00:20:34 -0300

    create banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+    <div className='relative h-full lg:h-[140vh]'>
+      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
+        <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
+          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+            <Image src='/banner.jpg' alt='the witcher' fill={true} className="h-[65vh] object-cover object-top lg:h-[95vh]"/>
+          </div>
+
+          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
+            The Witcher
+          </h1>
+
+          <p className='max-w-xs text-xs shadow-md md:max-w-lg md:text-lg lg:max-w-2xl'>
+            Beralt of Rivia, a solitary monster hunter, struggles to find his
+            place in a world where people often prove more wicked than beasts.
+          </p>
+        </div>
+
+        <div className='flex space-x-3'>
+          <button className='flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
+            Play
+          </button>
+          <button className='flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
+            More Info
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
