87566160f6d0847997bbcd31f1dddf5c2cc28e4c
Carlos Cardoso <puro_cros@hotmail.com>
2024-12-10 00:00:32 -0300

    change scrolled header background

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,29 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 0) {
+        setIsScrolled(true)
+      }else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  },[])
+
   return(
-    <header className="fixed top-0 z-50 flex w-full items-center justify-between transition-all lg:px-10 lg:py-6">
+    <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between transition-all lg:px-10 lg:py-6`}>
       <div className='flex space-x-2 md:space-x-8'>
         <Image src='/logo.png' alt='logo da netflix' width={120} height={120}/>
         <ul className='hidden md:flex items-center md:space-x-4 font-bold'>
