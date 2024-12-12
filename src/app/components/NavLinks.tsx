06624def67eb088f197a3b6d10a3cd25d39c6d85
Carlos Cardoso <puro_cros@hotmail.com>
2024-12-11 21:56:42 -0300

    cretae nav links component

---
+++ src/app/components/NavLinks.tsx
@@ -0,0 +1,10 @@
+export const NavLinks = () => (
+  <nav>
+    <ul className='hidden md:flex md:space-x-4 font-bold'>
+      <li>Home</li>
+      <li>TV Shows</li>
+      <li>Movies</li>
+      <li>Latest</li>
+    </ul>
+  </nav>
+);
