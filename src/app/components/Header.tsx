b1267a7f7e35c6a7e27871327ce65d8b538acd2c
Carlos Cardoso <puro_cros@hotmail.com>
2024-12-11 21:59:41 -0300

    refactor header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,42 +1,20 @@
 'use client'
 
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import UserProfile from './UserProfile'
+import Logo from './Logo'
+import { useScroll } from '../hooks/useScrool'
+import { NavLinks } from './NavLinks'
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if(window.scrollY > 0) {
-        setIsScrolled(true)
-      }else {
-        setIsScrolled(false)
-      }
-    }
-
-    window.addEventListener('scroll', handleScroll)
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  },[])
+  const isScrolled = useScroll()
 
   return(
     <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between transition-all lg:px-10 lg:py-6`}>
-      <div className='flex space-x-2 md:space-x-8'>
-        <Image src='/logo.png' alt='logo da netflix' width={120} height={120}/>
-        <ul className='hidden md:flex items-center md:space-x-4 font-bold'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+      <div className='flex items-center space-x-2 md:space-x-8'>
+        <Logo />
+        <NavLinks />
       </div>
-      <div className='flex items-center space-x-4'>
-        <p className='font-bold cursor-not-allowed lg:inline'>Kids</p>
-        <Image className='rounded cursor-pointer' src='/avatar-kids-profile.png' alt='logo perfil infantil' width={40} height={40}/>
-      </div>
+      <UserProfile />
     </header>
   )
 }
